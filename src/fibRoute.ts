56384c22eac2ef72ae856e2702644b4e1fef3b88
Hong Thy Nguyen <hongthy3101@gmail.com>
2025-08-24 17:30:46 -0400

    Add proper Express Request/Response type imports

--- src/fibRoute.ts
+++ src/fibRoute.ts
@@ -1,8 +1,9 @@
 // Endpoint for querying the fibonacci numbers
 
 import fibonacci from "./fib";
+import { Request, Response } from 'express';
 
-export default (req, res) => {
+export default (req: Request, res: Response): void => {
   const { num } = req.params;
 
   const fibN = fibonacci(parseInt(num));
