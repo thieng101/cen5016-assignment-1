fc80734ad6adf2efda5ae479cb33a5fd9da48be9
Hong Thy Nguyen <hongthy3101@gmail.com>
2025-08-24 17:33:14 -0400

    add explicit type annotation to num

--- src/fibRoute.ts
+++ src/fibRoute.ts
@@ -4,7 +4,7 @@ import fibonacci from "./fib";
 import { Request, Response } from 'express';
 
 export default (req: Request, res: Response): void => {
-  const { num } = req.params;
+  const num:string = req.params.num;
 
   const fibN = fibonacci(parseInt(num));
   let result = `fibonacci(${num}) is ${fibN}`;
